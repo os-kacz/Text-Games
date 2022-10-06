3e28ff8e1b99db13414c5105c0594f437b0555f8
oskacz <oskar2.kaczmarski@live.uwe.ac.uk>
2022-10-06 12:25:11 +0100

    updated main.cpp
    added pseudocode in comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
-
+/* PSEUDO
+ * 1. Generate a random number between 1-10
+ * 2. Ask the user to guess a number between 1-10
+ * 3. If the guess matches the random number, display "congratulations"
+ * 4. If the guess doesn't match the random number, display "game over"
+ * 5. End program
+ */
 int main()
 {
   std::cout << "\n ** Welcome to Console Games! ** \n" << std::endl;
