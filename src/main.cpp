a1146a87928ccf8ef201edd1933c22ce2ab6a86b
oskacz <oskar2.kaczmarski@live.uwe.ac.uk>
2022-10-08 21:16:50 +0100

    updated main.cpp
    changed opening bracket style

--- src/main.cpp
+++ src/main.cpp
@@ -14,26 +14,22 @@
  * 11. If yes, go to 1
  * 12. If no, end main instance loop and program
  */
-int main()
-{
+int main(){
   int guess_number = 0;
   bool running = true;
   bool winCond = false;
   char retry = 'y';
   std::cout << "Welcome to the number guessing game. The random number is between 1 and 10\n";
   // creates a loop that is broken when the player successfully guesses the random number
-  while (running)
-  {
+  while (running){
     // assigns a random number 1-10 to target_number
     // this is done in the while loop so a new number is generated per game instance
     int target_number = std::experimental::randint(1, 10);
-    for (int guess = 5; guess > 0; guess--)
-    {
+    for (int guess = 5; guess > 0; guess--){
       std::cout << "Guesses Left: " << guess << "\nGuess the random number:";
       std::cin >> guess_number;
       // if the player guesses the random number
-      if (target_number == guess_number)
-      {
+      if (target_number == guess_number){
         std::cout << "You guessed correct!";
         // guess is set to '0' to break the for loop and let the player win
         guess = 0;
@@ -41,33 +37,28 @@ int main()
         winCond = true;
       }
       // if the player guessed too high
-      if (target_number < guess_number)
-      {
+      if (target_number < guess_number){
         std::cout << "Nope, your guess was too high.\n";
       }
       // if the player guessed too low
-      if (target_number > guess_number)
-      {
+      if (target_number > guess_number){
         std::cout << "Nope, your guess was too low.\n";
       }
     }
     // when win condition is true, display win endgame text
-    if (winCond)
-    {
+    if (winCond){
       // win condition is set back to false in the event of the loop restarting
       winCond = false;
       std::cout << " You won!\nPlay again? (y/n):";
     }
     // when win condition is false, display game over endgame text
-    else
-    {
+    else{
       std::cout << "You've run out of guesses! The number was " << target_number
                 << ", Game Over!\nPlay again? (y/n):";
     }
     std::cin >> retry;
     // ends game if 'n'. otherwise, the loop restarts
-    if (retry == 'n')
-    {
+    if (retry == 'n'){
         running = false;
     }
   }
