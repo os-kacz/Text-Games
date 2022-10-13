50c7f35382e114fb7c955adce3fb94c7160ea22c
oskacz <oskar2.kaczmarski@live.uwe.ac.uk>
2022-10-13 12:40:30 +0100

    added rps game
    heavily unpolished

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <experimental/random>
+
 int main(){
   char menu = 'a';
   std::cout << "Welcome to Console Games\nPress 'g' to play the Random Number Guessing Game\nPress 'r' to play the Rock Paper Scissors Game\n:";
   std::cin >> menu;
+  bool running = true;
   if (menu == 'g')
   {
     int guess_number = 0;
-    bool running     = true;
     bool winCond     = false;
     char retry       = 'y';
     std::cout << "Welcome to the number guessing game. The random number is between 1 and 10\n";
@@ -17,6 +18,7 @@ int main(){
       // assigns a random number 1-10 to target_number
       // this is done in the while loop so a new number is generated per game instance
       int target_number = std::experimental::randint(1, 10);
+      // sets maximum guesses which is 5, and while you have guesses it loops. every loop removes a guess
       for (int guess = 5; guess > 0; guess--)
       {
         std::cout << "Guesses Left: " << guess << "\nGuess the random number:";
@@ -61,6 +63,78 @@ int main(){
         running = false;
       }
     }
+  }
+  else
+  {
+    int USRtally = 0;
+    int CPUtally = 0;
+    std::cout << "Welcome to the Rock Paper Scissors game\nPlease select:\n'r' for Rock\n'p' for Paper\n's' for Scissors\n";
+    while(running)
+    {
+      char USRraw = 'r';
+      // DEBUG CPU CHOICE BELOW
+      char CPUraw = 'r';
+      int USR = 0;
+      int CPU = std::experimental::randint(1,3);
+      std::cout << "Your Points: " << USRtally << "\nCPU's Points: " << CPUtally << "\n:";
+      std::cin >> USRraw;
+      switch(USRraw)
+      {
+        case 'r':
+          USR = 1;
+          break;
+        case 'p':
+          USR = 2;
+          break;
+        case 's':
+          USR = 3;
+          break;
+        case 'q':
+          running = false;
+          break;
+      }
+      // DEBUG CPU CHOICE BELOW
+      switch(CPU)
+      {
+        case 1:
+          CPUraw = 'r';
+          break;
+        case 2:
+          CPUraw = 'p';
+          break;
+        case 3:
+          CPUraw = 's';
+          break;
+      }
+      if (CPU == USR)
+      {
+        std::cout << "It's a tie! Neither of you got points\n";
+      }
+      else if ((CPU - USR * 1) > 1)
+      {
+        if (CPU > USR)
+        {
+          USRtally++;
+          std::cout << "You beat the CPU\n";
+        }
+        else
+        {
+          CPUtally++;
+          std::cout << "The CPU beat you with " << CPUraw << "\n";
+        }
+      }
+      else if (CPU > USR)
+      {
+        CPUtally++;
+        std::cout << "The CPU beat you with " << CPUraw << "\n";
+      }
+      else
+      {
+        USRtally++;
+        std::cout << "You beat the CPU\n";
+      }
+    }
+
   }
   return 0;
 }
